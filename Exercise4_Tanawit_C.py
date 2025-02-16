2e80db4d1c69a492377a96c55f818b4c11d3bd6d
Fri3b <ct.tanawit@gmail.com>
2025-02-16 22:18:16 +0700

    Exercise

---
+++ Exercise4_Tanawit_C.py
@@ -0,0 +1,10 @@
+
+Escore = input()
+GBscore = input()
+Comscore = input()
+
+print("--- Your Score ---")
+print("Foundation English : " + Escore)
+print("General Business : " + GBscore)
+print("Introduction to Computer Systems : " + Comscore)
+
