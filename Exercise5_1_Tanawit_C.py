0b7468902fd241ae211b2a07155dae45119b49e8
Fri3b <ct.tanawit@gmail.com>
2025-03-04 23:51:25 +0700

    Create Exercise5_1_Tanawit_C.py

---
+++ Exercise5_1_Tanawit_C.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a first number: "))
+y = int(input("Enter a second number: "))
+print(x,"+",y,"=",x+y)
+print(x,"-",y,"=",x-y)
+print(x,"*",y,"=",x*y)
+print(x,"/",y,"=",int(x/y))
